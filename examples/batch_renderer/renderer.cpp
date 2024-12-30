d0c2762d70ab18d9ac51dc574f0a61594f5b1829
Mohamed A.G <moonimooni132@gmail.com>
2024-12-30 08:22:26 +0200

    Better batching

--- examples/batch_renderer/renderer.cpp
+++ examples/batch_renderer/renderer.cpp
@@ -1,5 +1,6 @@
 #include "renderer.h"
 #include "nikol/nikol_core.hpp"
+#include "shaders.h"
 
 #include <vector>
 
@@ -9,7 +10,7 @@
 
 /// --------------------------------------------------------------------------------------
 /// DEFS
-#define MAX_QUADS     10000
+#define MAX_QUADS     1000
 #define MAX_VERTICES  MAX_QUADS * 4
 #define MAX_INDICES   MAX_QUADS * 6
 /// DEFS
@@ -21,9 +22,18 @@ struct Vertex {
   glm::vec3 pos; 
   glm::vec2 texture_coords;
   glm::vec4 color;
-  float texture_index;
 };
 /// Vertex 
+/// --------------------------------------------------------------------------------------
+
+/// --------------------------------------------------------------------------------------
+/// BatchDrawCall 
+struct BatchDrawCall {
+  nikol::sizei indices_count = 0; 
+  nikol::GfxTexture* texture = nullptr;
+  std::vector<Vertex> vertices;
+};
+/// BatchDrawCall 
 /// --------------------------------------------------------------------------------------
 
 /// --------------------------------------------------------------------------------------
@@ -32,13 +42,10 @@ struct Renderer {
   nikol::GfxContextDesc gfx_desc;
   nikol::GfxContext* gfx = nullptr;
 
-  nikol::GfxBufferDesc buff_desc;
-  nikol::GfxBuffer* vertex_buff = nullptr; 
-
   nikol::GfxPipelineDesc pipe_desc;
   nikol::GfxPipeline* pipe = nullptr;
 
-  std::vector<Vertex> vertices;
+  std::vector<BatchDrawCall> batches;
 
   glm::vec4 quad_vertices[4]; 
   glm::mat4 ortho_cam;
@@ -63,96 +70,21 @@ static void init_context(nikol::Window* window) {
 
 static const char* compile_shaders() {
 #if defined(NIKOL_GFX_CONTEXT_OPENGL)
-  return 
-    "#version 460 core\n"
-    "\n"
-    "// Layouts\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec2 aTextureCoords;\n"
-    "layout (location = 2) in vec4 aColor;\n"
-    "layout (location = 3) in float aTextureIndex;\n"
-    "\n"
-    "// Outputs\n"
-    "out VS_OUT {\n"
-    "  vec4 out_color;\n"
-    "  vec2 tex_coords;\n"
-    "  float tex_index;\n"
-    "} vs_out;\n"
-    "\n"
-    "void main() {\n"
-    "  vs_out.out_color  = aColor;\n"
-    "  vs_out.tex_coords = aTextureCoords;\n"
-    "  vs_out.tex_index  = aTextureIndex;\n"
-    "\n"
-    "  gl_Position = vec4(aPos, 1.0f);\n"
-    "}\n"
-    "\n"
-    "#version 460 core\n"
-    "\n"
-    "// Outputs\n"
-    "layout (location = 0) out vec4 frag_color;\n"
-    "\n"
-    "// Inputs\n"
-    "in VS_OUT {\n"
-    "  vec4 out_color;\n"
-    "  vec2 tex_coords;\n"
-    "  float tex_index;\n"
-    "} fs_in;\n"
-    "\n"
-    "// Uniforms\n"
-    "uniform sampler2D u_textures[32];\n"
-    "\n"
-    "void main() {\n"
-    "  int index = int(fs_in.tex_index);\n"
-    "  frag_color = texture(u_textures[index], fs_in.tex_coords) * fs_in.out_color;\n"
-    "}\n";
+  return batch_glsl_code();
 #elif defined(NIKOL_GFX_CONTEXT_DX11)
-  return 
-    "struct vs_in {\n"
-    "    float3 position   : POS;\n"
-    "    float2 tex_coords : TEX;\n"
-    "    float4 color      : COLOR;\n"
-    "    int tex_index     : INDEX;\n"
-    "};\n"
-    "\n"
-    "struct vs_out {\n"
-    "    float4 position   : SV_POSITION;\n"
-    "    float2 tex_coords : TEX;\n"
-    "    float4 color      : COLOR;\n"
-    "    int tex_index     : INDEX;\n"
-    "};\n"
-    "\n"
-    "Texture2D textures[32]     : register(t0);\n"
-    "SamplerState samp          : register(s0);\n"
-    "\n"
-    "vs_out vs_main(vs_in input) {\n"
-    "    vs_out output = (vs_out)0;\n"
-    "\n"
-    "    output.position   = float4(input.position, 1.0);\n"
-    "    output.tex_coords = input.tex_coords;\n"
-    "    output.color      = input.color;\n"
-    "    output.tex_index  = input.tex_index;\n"
-    "\n" 
-    "    return output;\n"
-    "}\n"
-    "\n"
-    "float4 ps_main(vs_out input) : SV_TARGET {\n"
-    "  float4 color;\n"
-    "  color = textures[1].Sample(samp, input.tex_coords) * input.color;\n"
-    "  return color;\n"
-    "}\n";
+  return batch_hlsl_code();
 #endif
 }
 
 static void init_pipeline() {
   // Vertex buffer init
-  s_renderer.buff_desc = {
+  nikol::GfxBufferDesc buff_desc = {
     .data  = nullptr, 
     .size  = sizeof(Vertex) * MAX_VERTICES, 
     .type  = nikol::GFX_BUFFER_VERTEX, 
     .usage = nikol::GFX_BUFFER_USAGE_DYNAMIC_DRAW,
   };
-  s_renderer.pipe_desc.vertex_buffer = nikol::gfx_buffer_create(s_renderer.gfx, s_renderer.buff_desc);
+  s_renderer.pipe_desc.vertex_buffer = nikol::gfx_buffer_create(s_renderer.gfx, buff_desc);
 
   // Index buffer init
   nikol::u32 indices[MAX_INDICES];
@@ -183,45 +115,34 @@ static void init_pipeline() {
   s_renderer.pipe_desc.layout[0] = nikol::GfxLayoutDesc{"POS", nikol::GFX_LAYOUT_FLOAT3, 0};
   s_renderer.pipe_desc.layout[1] = nikol::GfxLayoutDesc{"TEX", nikol::GFX_LAYOUT_FLOAT2, 0};
   s_renderer.pipe_desc.layout[2] = nikol::GfxLayoutDesc{"COLOR", nikol::GFX_LAYOUT_FLOAT4, 0};
-  s_renderer.pipe_desc.layout[3] = nikol::GfxLayoutDesc{"INDEX", nikol::GFX_LAYOUT_FLOAT1, 0};
-  s_renderer.pipe_desc.layout_count = 4;
+  s_renderer.pipe_desc.layout_count = 3;
 
   // Draw mode init 
   s_renderer.pipe_desc.draw_mode = nikol::GFX_DRAW_MODE_TRIANGLE;
 
-  // Textures init
-  nikol::u32 pixels = 0xffffffff;
-  nikol::GfxTextureDesc texture_desc = {
-    .width     = 1, 
-    .height    = 1, 
-    .depth     = 0, 
-    .format    = nikol::GFX_TEXTURE_FORMAT_RGBA8, 
-    .filter    = nikol::GFX_TEXTURE_FILTER_MIN_MAG_NEAREST, 
-    .wrap_mode = nikol::GFX_TEXTURE_WRAP_REPEAT, 
-    .data      = &pixels,
-  };
-  s_renderer.pipe_desc.textures[0]    = nikol::gfx_texture_create(s_renderer.gfx, texture_desc);
-  s_renderer.pipe_desc.textures_count = 1;
-
-  // For GLSL, we need to set the textures
-  int tex_ids[nikol::TEXTURES_MAX]; 
-  for(int i = 0; i < nikol::TEXTURES_MAX; i++) {
-    tex_ids[i] = i;
-  }
-  nikol::u32 uniform_loc = nikol::gfx_glsl_get_uniform_location(s_renderer.pipe_desc.shader, "u_textures");
-  nikol::gfx_glsl_upload_uniform_array(s_renderer.pipe_desc.shader, uniform_loc, nikol::TEXTURES_MAX, nikol::GFX_LAYOUT_INT1, tex_ids);
+  // s_renderer.pipe_desc.textures[0]    = nikol::gfx_texture_create(s_renderer.gfx, texture_desc);
 
   // Pipeline init
   s_renderer.pipe = nikol::gfx_pipeline_create(s_renderer.gfx, s_renderer.pipe_desc);
 }
 
-static void flush_renderer() {
+static void flush_batch(BatchDrawCall& draw_call) {
+  // An empty vertices array is useless... 
+  if(draw_call.vertices.size() == 0) {
+    return;
+  }
+
+  // Set the pipeline according to the draw call
+  s_renderer.pipe_desc.indices_count    = draw_call.indices_count;
+  s_renderer.pipe_desc.textures[0]      = draw_call.texture;
+  s_renderer.pipe_desc.textures_count   = 1;
+
   // Update the vertex buffer 
   nikol::gfx_buffer_update(s_renderer.gfx, 
                            s_renderer.pipe_desc.vertex_buffer, 
                            0, 
-                           sizeof(Vertex) * s_renderer.vertices.size(), 
-                           s_renderer.vertices.data());
+                           sizeof(Vertex) * draw_call.vertices.size(), 
+                           draw_call.vertices.data());
 
   // Apply the pipeline 
   nikol::gfx_context_apply_pipeline(s_renderer.gfx, s_renderer.pipe, s_renderer.pipe_desc);
@@ -229,18 +150,18 @@ static void flush_renderer() {
   // Draw the pipeline
   nikol::gfx_pipeline_draw_index(s_renderer.gfx, s_renderer.pipe);
   
-  // Reset the indices and the textures
-  s_renderer.pipe_desc.indices_count  = 0;
-  s_renderer.pipe_desc.textures_count = 1;
+  // Reset back to normal
+  draw_call.indices_count  = 0;
+  draw_call.vertices.clear();
 }
 
 static int find_texture(const nikol::GfxTexture* texture) {
   int index = -1;
 
-  for(int i = 0; i < s_renderer.pipe_desc.textures_count; i++) {
-    nikol::GfxTexture* pipe_tex = s_renderer.pipe_desc.textures[i];
+  for(int i = 0; i < s_renderer.batches.size(); i++) {
+    nikol::GfxTexture* batch_texture = s_renderer.batches[i].texture;
 
-    if(pipe_tex != texture) {
+    if(batch_texture != texture) {
       continue;
     }
       
@@ -260,6 +181,26 @@ static int find_texture(const nikol::GfxTexture* texture) {
 void renderer_create(nikol::Window* window) {
   init_context(window);
   init_pipeline();
+
+  // Allocate some memory for the batches
+  s_renderer.batches.reserve(32);
+
+  // Create a white texture batch  
+  nikol::u32 pixels = 0xffffffff;
+  nikol::GfxTextureDesc texture_desc = {
+    .width     = 1, 
+    .height    = 1, 
+    .depth     = 0, 
+    .format    = nikol::GFX_TEXTURE_FORMAT_RGBA8, 
+    .filter    = nikol::GFX_TEXTURE_FILTER_MIN_MAG_NEAREST, 
+    .wrap_mode = nikol::GFX_TEXTURE_WRAP_REPEAT, 
+    .data      = &pixels,
+  };
+  BatchDrawCall draw_call = {
+    .indices_count = 0, 
+    .texture       = nikol::gfx_texture_create(s_renderer.gfx, texture_desc),
+  };
+  s_renderer.batches.push_back(draw_call); 
   
   // Quad vertices init
   s_renderer.quad_vertices[0] = glm::vec4(-0.5f, -0.5f, 0.0f, 1.0f);
@@ -282,71 +223,68 @@ void renderer_begin() {
   nikol::window_get_size(s_renderer.gfx_desc.window, &width, &height);
 
   s_renderer.ortho_cam = glm::ortho(0.0f, (float)width, (float)height, 0.0f);
-  s_renderer.vertices.clear();
 }
 
 void renderer_end() {
-  flush_renderer();
+  for(auto& batch : s_renderer.batches) {
+    flush_batch(batch);
+  }
 
   nikol::gfx_context_present(s_renderer.gfx);
 }
 
 void render_texture(nikol::GfxTexture* texture, const glm::vec2& pos, const glm::vec2& size, const glm::vec4& tint) {
-  bool can_flush = s_renderer.pipe_desc.indices_count >= MAX_INDICES || s_renderer.pipe_desc.textures_count >= nikol::TEXTURES_MAX;
-  if(can_flush) {
-    flush_renderer();
-  }
+  int index                = find_texture(texture);
+  BatchDrawCall* draw_call = &s_renderer.batches[index];
 
-  int index = find_texture(texture);
-  
-  // Adding the texture to be drawn later if it's new
-  if(index == -1) {
-    index = s_renderer.pipe_desc.textures_count++;
-    s_renderer.pipe_desc.textures[index] = texture;
+  if(draw_call->indices_count >= MAX_INDICES) {
+    flush_batch(*draw_call);
   }
 
   glm::mat4 model     = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) * 
                         glm::scale(glm::mat4(1.0f), glm::vec3(size.x, size.y, 0.0f));
-  glm::mat4 world_pos = s_renderer.ortho_cam * model;
+  glm::mat4 world_pos =  s_renderer.ortho_cam * model;
 
   // Top-left 
-  Vertex v1; 
-  v1.pos            = world_pos * s_renderer.quad_vertices[0]; 
-  v1.color          = tint;
-  v1.texture_coords = pos / size; 
-  v1.texture_index  = index;
-  s_renderer.vertices.push_back(v1);
- 
-  // Top-right
-  Vertex v2; 
-  v2.pos            = world_pos * s_renderer.quad_vertices[1]; 
-  v2.color          = tint;
-  v2.texture_coords = glm::vec2(pos.x + size.x, pos.y) / size; 
-  v2.texture_index  = index;
-  s_renderer.vertices.push_back(v2);
- 
-  // Bottom-right
-  Vertex v3; 
-  v3.pos            = world_pos * s_renderer.quad_vertices[2]; 
-  v3.color          = tint;
-  v3.texture_coords = (pos + size) / size; 
-  v3.texture_index  = index;
-  s_renderer.vertices.push_back(v3);
- 
-  // Bottom-left
-  Vertex v4; 
-  v4.pos            = world_pos * s_renderer.quad_vertices[3]; 
-  v4.color          = tint;
-  v4.texture_coords = glm::vec2(pos.x, pos.y + size.y) / size; 
-  v4.texture_index  = index;
-  s_renderer.vertices.push_back(v4);
+  Vertex v1 = {
+    .pos            = world_pos * s_renderer.quad_vertices[0], 
+    .texture_coords = pos / size, 
+    .color          = tint,
+  };
+  draw_call->vertices.push_back(v1);
 
-  s_renderer.pipe_desc.indices_count  += 6;
+  // Top-right
+  Vertex v2 = {
+    .pos            = world_pos * s_renderer.quad_vertices[1], 
+    .texture_coords = glm::vec2(pos.x + size.x, pos.y) / size, 
+    .color          = tint,
+  };
+  draw_call->vertices.push_back(v2);
+
+  // Bottom-right
+  Vertex v3 = {
+    .pos            = world_pos * s_renderer.quad_vertices[2], 
+    .texture_coords = (pos + size) / size, 
+    .color          = tint,
+  }; 
+  draw_call->vertices.push_back(v3);
+
+  // Bottom-left
+  Vertex v4 = {
+    .pos            = world_pos * s_renderer.quad_vertices[3], 
+    .texture_coords = glm::vec2(pos.x, pos.y + size.y) / size, 
+    .color          = tint,
+  }; 
+  draw_call->vertices.push_back(v4);
+
+  draw_call->indices_count  += 6;
 }
 
 void render_quad(const glm::vec2& pos, const glm::vec2& size, const glm::vec4& color) {
-  if(s_renderer.pipe_desc.indices_count >= MAX_INDICES) {
-    flush_renderer();
+  BatchDrawCall* draw_call = &s_renderer.batches[0];
+
+  if(draw_call->indices_count >= MAX_INDICES) {
+    flush_batch(*draw_call);
   }
 
   glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x, pos.y, 0.0f)) * 
@@ -354,42 +292,43 @@ void render_quad(const glm::vec2& pos, const glm::vec2& size, const glm::vec4& c
   glm::mat4 world_pos =  s_renderer.ortho_cam * model;
 
   // Top-left 
-  Vertex v1; 
-  v1.pos            = world_pos * s_renderer.quad_vertices[0]; 
-  v1.color          = color;
-  v1.texture_coords = glm::vec2(0.0f, 0.0f); 
-  v1.texture_index  = 0;
-  s_renderer.vertices.push_back(v1);
+  Vertex v1 = {
+    .pos            = world_pos * s_renderer.quad_vertices[0],
+    .texture_coords = glm::vec2(0.0f, 0.0f),
+    .color          = color,
+  }; 
+  draw_call->vertices.push_back(v1);
 
   // Top-right
-  Vertex v2; 
-  v2.pos            = world_pos * s_renderer.quad_vertices[1]; 
-  v2.color          = color;
-  v2.texture_coords = glm::vec2(1.0f, 0.0f); 
-  v2.texture_index  = 0;
-  s_renderer.vertices.push_back(v2);
+  Vertex v2 = {
+    .pos            = world_pos * s_renderer.quad_vertices[1], 
+    .texture_coords = glm::vec2(1.0f, 0.0f), 
+    .color          = color,
+  };
+  draw_call->vertices.push_back(v2);
 
   // Bottom-right
-  Vertex v3; 
-  v3.pos            = world_pos * s_renderer.quad_vertices[2]; 
-  v3.color          = color;
-  v3.texture_coords = glm::vec2(1.0f, 1.0f); 
-  v3.texture_index  = 0;
-  s_renderer.vertices.push_back(v3);
+  Vertex v3 = {
+    .pos            = world_pos * s_renderer.quad_vertices[2], 
+    .texture_coords = glm::vec2(1.0f, 1.0f), 
+    .color          = color,
+  };
+  draw_call->vertices.push_back(v3);
 
   // Bottom-left
-  Vertex v4; 
-  v4.pos            = world_pos * s_renderer.quad_vertices[3]; 
-  v4.color          = color;
-  v4.texture_coords = glm::vec2(0.0f, 1.0f); 
-  v4.texture_index  = 0;
-  s_renderer.vertices.push_back(v4);
+  Vertex v4 = {
+    .pos            = world_pos * s_renderer.quad_vertices[3], 
+    .texture_coords = glm::vec2(0.0f, 1.0f), 
+    .color          = color,
+  };
+  draw_call->vertices.push_back(v4);
 
-  s_renderer.pipe_desc.indices_count += 6;
+  draw_call->indices_count += 6;
 }
 
 nikol::GfxTexture* renderer_load_texture(const char* path) {
-  nikol::GfxTextureDesc desc; 
+  nikol::GfxTextureDesc desc = {}; 
+  nikol::GfxTexture* texture = nullptr;
 
   int width, height, channels;
 
@@ -404,8 +343,17 @@ nikol::GfxTexture* renderer_load_texture(const char* path) {
   desc.filter    = nikol::GFX_TEXTURE_FILTER_MIN_MAG_NEAREST;
   desc.wrap_mode = nikol::GFX_TEXTURE_WRAP_REPEAT;
   desc.data      = pixels;
+  
+  texture = nikol::gfx_texture_create(s_renderer.gfx, desc);
+  
+  // Create a new batch for the texture 
+  BatchDrawCall draw_call = {
+    .indices_count = 0, 
+    .texture       = texture,
+  };
+  s_renderer.batches.push_back(draw_call);
 
-  return nikol::gfx_texture_create(s_renderer.gfx, desc);
+  return texture;
 }
 
 /// Public functions
